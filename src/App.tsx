066160dcddbf930dae0479fea4b82ef917152193
HabibMollah <hrmollah2002@gmail.com>
2023-05-23 03:05:18 +0600

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
           lg: `"nav nav" "sidebar main"`,
         }}>
         <GridItem bg={'coral'} area="nav">
-          Nav
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem bg={'brown'} area="sidebar">
