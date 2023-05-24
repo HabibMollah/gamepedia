df4f47d5bba110dfb75cc64429fbafdf882d3ac6
HabibMollah <hrmollah2002@gmail.com>
2023-05-24 21:33:51 +0600

    align spinner

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,26 +1,42 @@
-import { Text, HStack, Image, List, ListItem, Spinner } from '@chakra-ui/react';
+import {
+  Text,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  SimpleGrid,
+  GridItem,
+  Box,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import cropImage from '../services/cropImage';
 
 function GenresList() {
   const { data, isLoading, error } = useGenres();
   return (
-    <List>
-      {isLoading && <Spinner />}
-      {error && <p>{error}</p>}
-      {data.map((genre) => (
-        <ListItem paddingY="5px" key={genre.id}>
-          <HStack>
-            <Image
-              boxSize={'32px'}
-              borderRadius={8}
-              src={cropImage(genre.image_background)}
-            />
-            <Text fontSize="lg">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && (
+        <Box marginTop={16} textAlign={'center'}>
+          <Spinner size={'xl'} />
+        </Box>
+      )}
+      <List>
+        {error && <p>{error}</p>}
+        {data.map((genre) => (
+          <ListItem paddingY="5px" key={genre.id}>
+            <HStack>
+              <Image
+                boxSize={'32px'}
+                borderRadius={8}
+                src={cropImage(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 
