3e9fe9a40e58a489ef78e470333bb726837e345d
HabibMollah <hrmollah2002@gmail.com>
2023-05-24 14:15:40 +0600

    fine tune loading skeletons

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,10 +1,12 @@
-import { Card, Skeleton, SkeletonText } from '@chakra-ui/react';
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
 
 function GameCardSkeleton() {
   return (
     <Card width={'300px'}>
-      <Skeleton height={'250px'} overflow={'hidden'} borderRadius={'10px'} />
-      <SkeletonText padding={4} />
+      <Skeleton height={'250px'} overflow={'hidden'} borderRadius={10} />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
     </Card>
   );
 }
