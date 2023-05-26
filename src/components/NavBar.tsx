c9b579dbcd95885ec0885f825793253ffbcb8ff7
HabibMollah <hrmollah2002@gmail.com>
2023-05-26 08:22:30 +0600

    add searchbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
 import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import DarkModeSwitch from './DarkModeSwitch';
+import SearchBar from './SearchBar';
 function NavBar() {
   return (
-    <HStack justifyContent={'space-between'} padding={'10px'}>
+    <HStack padding={'10px'}>
       <Image
         src={logo}
         alt="GamePedia logo"
         boxSize="50px"
         borderRadius={100}
       />
+      <SearchBar />
       <DarkModeSwitch />
     </HStack>
   );
