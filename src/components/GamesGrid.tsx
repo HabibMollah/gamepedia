9516ead19cbaa88bf20d46ace89275c54ca2e7a9
HabibMollah <hrmollah2002@gmail.com>
2023-05-26 15:17:48 +0600

    fine tune theme and game grid

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -13,31 +13,30 @@ function GamesGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <div>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}
-        spacing={6}
-        padding="10px">
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {!isLoading &&
-          data.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard
-                game={game}
-                platforms={game.parent_platforms}
-                metacritic={game.metacritic}
-              />
-            </GameCardContainer>
-          ))}
-      </SimpleGrid>
-    </div>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}
+      spacing={6}
+      padding="10px">
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {!isLoading &&
+        data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard
+              game={game}
+              platforms={game.parent_platforms}
+              metacritic={game.metacritic}
+            />
+          </GameCardContainer>
+        ))}
+    </SimpleGrid>
   );
 }
 
